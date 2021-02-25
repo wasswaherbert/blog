e27199eb54cbba209159b94651d6cec13b98c727
herbert <wasswaherbert907@gmail.com>
2021-02-25 12:30:07 +0300

    just added a readme file and log entries

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': 'blog@blog',
+        'PASSWORD': 'blog',
         'HOST' : 'localhost',
         'PORT' : '',
     }
