ec82e5574782946a07be3e87fd3e331050d7eb6b
herbert <wasswaherbert907@gmail.com>
2021-06-21 08:47:19 +0300

    updated blog
    with front end updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': 'blog',
+        'PASSWORD': 'blog@blog',
         'HOST' : 'localhost',
         'PORT' : '',
     }
